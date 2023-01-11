71a9d32e50f363d1ccfab0d617cba9cd6ff65e6b
wmcentire <wmcentire@student.neumont.edu>
2023-01-11 11:31:08 -0700

    Added turning and made the scene space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Range(1,10)] public float speed = 5;
+    [Range(1,20)] public float speed = 5;
+    [Range(1,90)]public float rotationRate = 60;
     public GameObject prefab;
 
     // Start is called before the first frame update
@@ -24,16 +25,26 @@ public class Player : MonoBehaviour
         //transform.position = new Vector3(2, 2, 2);
         //transform.rotation = Quaternion.Euler(20f, 40f, 10f);
         //transform.localScale = Vector3.one * Random.value * 5;
-        Vector3 direction = Vector3.zero;
-
-        direction.x = Input.GetAxis("Horizontal");
-        direction.z = Input.GetAxis("Vertical");
-
         //if (Input.GetKey(KeyCode.W)) direction.z = 1;
         //if (Input.GetKey(KeyCode.S)) direction.z = -1;
         //if (Input.GetKey(KeyCode.A)) direction.x = -1;
         //if (Input.GetKey(KeyCode.D)) direction.x = 1;
-        transform.position += direction * speed * Time.deltaTime * 5;
+
+
+
+        Vector3 direction = Vector3.zero;
+
+        //direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = Input.GetAxis("Horizontal");
+
+        Quaternion rotate = Quaternion.Euler(rotation * rotationRate * Time.deltaTime);
+        transform.rotation = transform.rotation * rotate;
+
+        transform.Translate(direction * speed * Time.deltaTime);
+        //transform.position += direction * speed * Time.deltaTime * 5;
 
         if (Input.GetButtonDown("Fire1"))
         {
