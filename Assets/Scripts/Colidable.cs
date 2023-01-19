7bf8ef8bfe1fb22868c2f95e89d69626f407b546
wmcentire <wmcentire@student.neumont.edu>
2023-01-19 11:36:03 -0700

    added onExit and onStay

--- Assets/Scripts/Colidable.cs
+++ Assets/Scripts/Colidable.cs
@@ -19,11 +19,43 @@ public class Colidable : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (hitTagName == string.Empty || other.gameObject.CompareTag(hitTagName))
+        {
+            onExit?.Invoke(other.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (hitTagName == string.Empty || other.gameObject.CompareTag(hitTagName))
+        {
+            onStay?.Invoke(other.gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(hitTagName == string.Empty || collision.gameObject.CompareTag(hitTagName))
         {
             onEnter?.Invoke(collision.gameObject);  
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (hitTagName == string.Empty || collision.gameObject.CompareTag(hitTagName))
+        {
+            onExit?.Invoke(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (hitTagName == string.Empty || collision.gameObject.CompareTag(hitTagName))
+        {
+            onStay?.Invoke(collision.gameObject);
         }
     }
 }
