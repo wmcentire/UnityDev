0362f6bb3be5cf18dbf7dd1f0c6270934d3147f3
wmcentire <wmcentire@student.neumont.edu>
2023-01-19 11:09:17 -0700

    Added following Camera

--- Assets/Scripts/RollerCamera.cs
+++ Assets/Scripts/RollerCamera.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class RollerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform target;
+    [SerializeField, Range(2,20)] private float distance = 2;
+    [SerializeField, Range(2,20)] private float offset = 2;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + (Vector3.up * distance) + (Vector3.back * offset);
+        transform.rotation = Quaternion.LookRotation(target.transform.position);
     }
 }
